16442fe97da73d2307e5b07c58a61be7c7797aff
Christian Fässler <cfaessle@hsr.ch>
2014-08-13 09:07:17 +0200

    Update build_lamp.sh

--- build_lamp.sh
+++ build_lamp.sh
@@ -1,17 +1,10 @@
 #! /bin/sh
+# /etc/init.d/BuildLamp
+
 SCRIPT_PATH="/usr/local/bin"
 SCRIPT_NAME="build_lamp_loop.py"
 NAME="BuildLamp"
 PIDFILE=/var/run/$NAME.pid
-
-### BEGIN INIT INFO
-# Provides:          build lamp service
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Simple script to start build lamp deamon at startup
-### END INIT INFO
 
 case "$1" in
   start)
